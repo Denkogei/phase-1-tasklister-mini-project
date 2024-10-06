833d1e276e1d6aa0d9b63efba6d61714b457b3a0
Dennis Kemboi <denkogei11@gmail.com>
2024-10-06 14:36:03 +0300

    mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let form =document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createTodo(e.target["new-task-description"].value)
+    const priority = e.target["priority"].value;
+    form.reset()
+  })
+})
+
+function createTodo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  p.textContent = ` ${todo}  `
+  btn.textContent = ' X'
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
